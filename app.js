f6e6ce4b0401328418d25dbae7b3e99945715d4c
殷凌越 <yin_lingyue@163.com>
2017-07-19 16:49:53 +0800

    change

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
-//app.js
 App({
-	onLaunch: function () {
-		//调用API从本地缓存中获取数据
-		var logs = wx.getStorageSync('logs') || []
-		logs.unshift(Date.now())
-		wx.setStorageSync('logs', logs)
+
+	globalData: {
+		deviceInfo: {}
 	},
-	getUserInfo:function(cb){
-		var that = this
-		if(this.globalData.userInfo){
-			typeof cb == "function" && cb(this.globalData.userInfo)
-		}else{
-			//调用登录接口
-			wx.login({
-				success: function () {
-					wx.getUserInfo({
-						success: function (res) {
-							that.globalData.userInfo = res.userInfo
-							typeof cb == "function" && cb(that.globalData.userInfo)
-						}
-					})
-				}
-			})
-		}
-	},
-	globalData:{
-		userInfo:null
-	}
+
+    onLaunch: function() {
+        try {
+     		var localInfo = wx.getStorageSync("deviceInfo");
+
+        	if (!localInfo) {
+        		var res = wx.getSystemInfoSync();
+        		wx.setStorageSync("deviceInfo", res);
+        		this.globalData.deviceInfo = res;
+        	}else {
+        		this.globalData.deviceInfo = localInfo;
+        	}
+
+		} catch (e) {}
+    }
+
 })
